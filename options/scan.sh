af98139ad00c494a0158025d54e6063e97f32601
Evgeni Kurtev <evgenikurtev@abv.bg>
2025-06-23 09:58:46 +0200

    fix update

--- options/scan.sh
+++ options/scan.sh
@@ -4,6 +4,6 @@
 while IFS=: read -r username password uid gid comment homedir shell; do
     if [ "$uid" -ge 1000 ] && [ "$uid" -lt 30000 ] && ! grep -q "users.users.$username" "$CONFIGURATION_FILE" ;then
       echo "Found user $username!"
-      sh options/add-user.sh -s --name "$username" --home "$homedir" --uid "$uid" --description "$comment" --isNormalUser
+      sh "$NIX_USER_DIR"/options/add-user.sh -s --name "$username" --home "$homedir" --uid "$uid" --description "$comment" --isNormalUser
     fi
 done < /etc/passwd
